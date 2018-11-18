fa242c618bb2532f1f5bb954bccb1da55b2f2753
Inmaculada Pérez <inmape01@ucm.es>
2018-11-18 14:06:00 +0100

    Implementation
    main.cpp just for testing and debugging

---
+++ daryHeap.h
@@ -0,0 +1,151 @@
+/*
+	d-ary heap implementation based on binary heaps.
+
+	GitHub: @Inmapg
+*/
+#ifndef daryHeap_h
+#define daryHeap_h
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <stdexcept>
+
+// Comparator used to establish the priority
+template <typename T = int, typename Comparator = std::less<T>>
+class daryHeap {
+
+	// Contais the data
+	std::vector<T> array;
+
+	int numChildren;
+
+	// Object that decides which element of the comparison is firstly leaving the heap.
+	Comparator before;
+
+public:
+	daryHeap(int numCh = 1, Comparator c = Comparator()) : array(0), numChildren(numCh), before(c) {}
+
+	daryHeap(daryHeap<T, Comparator> const&) = default;
+
+	daryHeap<T, Comparator> & operator=(daryHeap<T, Comparator> const&) = default;
+
+	~daryHeap() = default;
+
+	// Inserts the element x (which includes its priority).
+	void push(T const& x) {
+		array.push_back(x);
+		shiftUp(array.size() - 1);
+	}
+
+	// Returns the heap's size
+	int size() const { return array.size(); }
+
+	// Returns if the heap has no elements
+	bool empty() const { return size() == 0; }
+
+	// Returns the element with the major priority if the heap is not empty
+	T const& top()  const {
+		if (empty()) throw std::domain_error("The heap is empty.");
+		else return array[0];
+	}
+
+	// Removes from the heap the element with the major priority if the heap is not empty
+	void pop() {
+		T e = deleteAt(0);
+	}
+
+	// Returns and removes from the heap the element at the index 'ind' if the heap is not empty
+	T deleteAt(int ind) {
+		if (empty())
+			throw std::domain_error("You can't remove the first element due to the heap is empty.");
+		else if (ind >= size()) {
+			throw std::domain_error("Position out of range");
+		}
+		else {
+			T elem = array[ind];
+			array[ind] = array[array.size() - 1];
+			array.pop_back();
+			if (!empty() && ind < size()) shiftDown(ind);
+			return elem;
+		}
+	}
+
+	// Prints the current state of the heap (just for debugging)
+	void print(std::ostream & o) const {
+		if (!empty()) {
+			o << array[0];
+			for (auto i = 1; i < size(); ++i)
+				o << " | " << array[i];
+			o << '\n';
+		}
+	}
+
+	// Clears the heap (just for debugging)
+	void clear() {
+		array.clear();
+	}
+
+private:
+
+	// Shifts up the element located on the position 'i' of the heap
+	void shiftUp(int i) {
+		T elem = array[i];
+		int place = i;
+		while (place > 0 && before(elem, array[parent(place)])) {
+			array[place] = array[parent(place)];
+			place = parent(place);
+		}
+		array[place] = elem;
+	}
+
+	// Shifts down the element located on the position 'i' of the heap
+	void shiftDown(int i) {
+		T elem = array[i];
+		int place = i;
+		int child; // left one
+		bool end = false;
+		while (getIndexK(place, 1) <= size() && !end) {
+			child = minChild(place);
+			// change child
+			if (child < size() && before(array[child + 1], array[child]))
+				++child;
+			// shift up the child if it goes before the element that is shifting down
+			if (before(array[child], elem)) {
+				array[place] = array[child];
+				place = child;
+			}
+			else end = true;
+		}
+		array[place] = elem;
+	}
+
+	int minChild(int i) {
+		int minimum = getIndexK(i, 1);
+		int k = 2;
+		int pos = getIndexK(i, k);
+		while (k <= numChildren && pos < array.size()) {
+			if (array[pos] < array[minimum]) {
+				minimum = pos;
+			}
+			pos = getIndexK(i, k++);
+		}
+		return minimum;
+	}
+
+	// Returns the k th child
+	int getIndexK(int child, int k) {
+		return numChildren * child + k;
+	}
+
+	int parent(int child) {
+		return (child - 1) / numChildren;
+	}
+};
+
+template <typename T, typename Comparator>
+inline std::ostream& operator<<(std::ostream & o, daryHeap<T, Comparator> const& a) {
+	a.print(o);
+	return o;
+}
+#endif
---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include "daryHeap.h"
+
+// Just for debugging and testing
+int main() {
+	int numChildren, size;
+	std::cout << ">>> D-ary Heap test <<<\n------------------------\n";
+	std::cout << "Enter the number of children each node will have: ";
+	std::cin >> numChildren;
+	daryHeap<int> testHeap(numChildren);
+	bool exit = false;
+	do {
+		std::cout << "\n______________________________________________\nOperations:\n";
+		std::cout << "\t1) Insert element in the heap\n";
+		std::cout << "\t2) Delete element from the heap\n";
+		std::cout << "\t3) Check empty heap\n";
+		std::cout << "\t4) Print size\n";
+		std::cout << "\t5) Print heap\n";
+		std::cout << "\t6) Clear heap\n";
+		std::cout << "\t7) Terminate the execution of the program\n";
+		std::cout << "Select option: ";
+		int choice;
+		std::cin >> choice;
+		std::cout << '\n';
+		switch (choice){
+			case 1: {
+				std::cout << "Enter element to insert: ";
+				int aux;
+				std::cin >> aux;
+				testHeap.push(aux);
+				std::cout << "\nCurrent heap status ==> ";
+				testHeap.print(std::cout);
+				break;
+			}
+			case 2: {
+				char answer;
+				std::cout << "Do you want to delete the first element? [y/n]: ";
+				std::cin >> answer;
+				if (answer == 'y') {
+					try {
+						int deletion = testHeap.top();
+						testHeap.pop();
+						std::cout << "Successfully deleted " << deletion << '\n';
+					}
+					catch (std::domain_error e) {
+						std::cout << e.what() << '\n';
+					}
+				}
+				else {
+					std::cout << "Enter delete position (1, ... , " << testHeap.size() << "): ";
+					int pos;
+					std::cin >> pos;
+					try {
+						int deletion = testHeap.deleteAt(pos-1);
+						std::cout << "Successfully deleted " << deletion << '\n';
+					}
+					catch (std::domain_error e) {
+						std::cout << e.what() << '\n';
+					}
+				}
+				std::cout << "\nCurrent heap status ==> ";
+				testHeap.print(std::cout);
+				break;
+			}
+			case 3: {
+				std::cout << "The heap is";
+				if (!testHeap.empty()) {
+					std::cout << " not";
+				}
+				std::cout << " empty\n";
+				break;
+			}
+			case 4: {
+				std::cout << "The size of the heap is " << testHeap.size() << '\n';
+				break;
+			}
+			case 5: {
+				testHeap.print(std::cout);
+				break;
+			}
+			case 6: {
+				testHeap.clear();
+				std::cout << "The heap has been successfully cleared. All elements have been removed from the heap.\n";
+				break;
+			}
+			case 7: {
+				exit = true;
+				break;
+			}
+			default: {
+				std::cout << "Option out of range. Please, choose an option between 1 and 7.\n";
+				break;
+			}
+		}
+	} while (exit == false);
+
+
+	return 0;
+}
